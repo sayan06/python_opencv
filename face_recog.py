52decc0aa4d0c911517aaf1a194e0858cd600bf1
Sayan Muhury <sayan@consilioussystems.com>
2020-03-06 16:25:58 +0530

    opencv2

--- face_recog.py
+++ face_recog.py
@@ -1,1 +1,15 @@
-print('Match')
+# import cv2
+# WINDOW_DELAYS = [
+#     3000,
+#     8000
+# ]
+# img = cv2.imread('lena.jpg', cv2.IMREAD_COLOR)
+# cv2.imshow('image', img)
+# cv2.imshow('image2', img)
+
+# for delay in WINDOW_DELAYS:
+#     cv2.waitKey (delay)
+#     cv2.destroyAllWindows()
+# # cv2.imshow('image', img)
+# # cv2.waitKey(WINDOW_DELAYS[1])
+# #1cv2.imwrite('lencopy.png', img)
---
+++ face_recog2.py
@@ -0,0 +1,35 @@
+#image reader writer
+# import cv2
+# img = cv2.imread('lena.jpg', cv2.IMREAD_COLOR)
+# cv2.imshow('image', img)
+# k = cv2.waitKey (0) & 0xFF
+# if   k == 27:
+#   cv2.destroyAllWindows()
+# elif k == ord('s'):
+#   cv2.imwrite('lena_copy.png', img)
+#   cv2.destroyAllWindows()
+
+
+  #shape
+import numpy as np
+import cv2
+
+#img = cv2.imread('lena.jpg', 1)
+img = 255*np.ones([512, 512, 3], np.uint8)
+#img.fill(120)
+
+img = cv2.line(img, (0,0), (255,255), (10, 96, 10), 10) # 44, 96, 147
+img = cv2.arrowedLine(img, (0,255), (255,255), (255, 0, 0), 10)
+img = cv2.rectangle(img, (384, 0), (510, 128), (0, 0, 255), 10)
+img = cv2.circle(img, (447, 63), 63, (0, 255, 0), -1)
+font = cv2.FONT_HERSHEY_SIMPLEX
+img = cv2.putText(img, 'OpenCv', (10, 500), font, 4, (0, 255, 255), 10, cv2.LINE_AA)
+# img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+# pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+# pts = pts.reshape((-1,1,2))
+# img = cv2.polylines(img,[pts],True,(0,255,255))
+cv2.imshow('image', img)
+
+cv2.waitKey(3000)
+cv2.destroyAllWindows() 
